2bc401b6c498f744dc9db1dad30ac98945ace920
Adam Ayd <adam.ayd@gmail.com>
2019-12-17 00:12:34 -0500

    added testing for circleci test

---
+++ src/pages/Index.test.js
@@ -0,0 +1,13 @@
+// import { mount } from '@vue/test-utils'
+// import Index from './Index.vue'
+
+describe('Index.vue', () => {
+  test('setup correctly', () => {
+    expect(true).toBe(true)
+  })
+  // test('contains hello text', () => {
+  //   const wrapper = mount(Index)
+  //   expect(wrapper.text()).toContain('Hello, world!')
+  // })
+})
+
